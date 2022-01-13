d9671b172991d5c0df0d634983e9cbe152c7cc9d
Glorzo <thisaraeranga@gmail.com>
2022-01-12 22:48:33 -0600

    added functionality to create a task entered by the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,21 +2,28 @@
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do"); 
 
+
+
 var createTaskHandler = function(event) {
 
     event.preventDefault();
-    
-    // Create a new task item
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+
+    // Create list item
     var listItemEL = document.createElement("li");
-
-    // Style the new task item
     listItemEL.className = "task-item";
+    
+    // create div to hold task info and add to list item
+    var taskInfoEL = document.querySelector("div");
+    taskInfoEL.className = "task-info";
 
-    // Add the text
-    listItemEL.textContent = "This is a new task.";
-
-    // Append this element to the task list
-    tasksToDoEl.appendChild(listItemEL);
+    // add HTML content to div
+    taskInfoEL.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+    listItemEL.appendChild(taskInfoEL);
+    
+    // add entire list item to list
+    tasksToDoEl.appendChild(listItemEL);    
 };
 
 formEl.addEventListener("submit", createTaskHandler);
