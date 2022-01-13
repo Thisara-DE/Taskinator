12628f517430594df69c91f003e5b2d4d169d60a
Glorzo <thisaraeranga@gmail.com>
2022-01-12 23:40:39 -0600

    add user input validation to not accept nulls and add reset to the form

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,6 +8,11 @@ var taskFormHandler = function(event) {
     var taskNameInput = document.querySelector("input[name='task-name']").value;
     var taskTypeInput = document.querySelector("select[name='task-type']").value;
 
+    if (!taskNameInput || !taskTypeInput) {
+        alert("You need to fill out the task form!");
+        return false;
+    }
+
     // package up data as an object
     var taskDataObj = {
         name: taskNameInput,
@@ -32,7 +37,9 @@ var createTaskEl = function (taskDataObj) {
     listItemEl.appendChild(taskInfoEl);
     
     // add entire list item to list
-    tasksToDoEl.appendChild(listItemEl);  
+    tasksToDoEl.appendChild(listItemEl); 
+    
+    formEl.reset();
 };
 
 formEl.addEventListener("submit", taskFormHandler);
